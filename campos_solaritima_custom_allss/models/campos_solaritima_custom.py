b4a4e303f00394c1afbbaddcedeb3e4c8cfcf1d1
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2021-03-17 17:45:30 -0300

    Commit correção

--- campos_solaritima_custom_allss/models/campos_solaritima_custom.py
+++ campos_solaritima_custom_allss/models/campos_solaritima_custom.py
@@ -7,7 +7,7 @@ class ResPartnerCustom(models.Model):
     _inherit = "res.partner"
 
     currency_id = fields.Many2one("res.currency", related="company_id.currency_id")
-    # _allss_teste = fields.Char("TESTE")
+    _allss_teste = fields.Char("TESTE")
     # _allss_wage = fields.Monetary("Salário") 
     # _allss_health_insurance = fields.Boolean("Convênio Médico")
     # _allss_date_exped_cnh = fields.Date("Data de Expedição")
