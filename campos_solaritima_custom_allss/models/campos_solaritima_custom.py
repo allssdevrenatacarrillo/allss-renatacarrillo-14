68558d6e6a01a0e7fd941a61d22f98269365839c
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2021-03-18 10:30:44 -0300

    Inclusão de Campos do Grupo Cliente

--- campos_solaritima_custom_allss/models/campos_solaritima_custom.py
+++ campos_solaritima_custom_allss/models/campos_solaritima_custom.py
@@ -8,6 +8,8 @@ class ResPartnerCustom(models.Model):
 
     currency_id = fields.Many2one("res.currency", related="company_id.currency_id")
     _allss_concessionaria = fields.Char("Concessionaria")
+    _allss_titular_inst = fields.Char("Copiar Cliente para Titular da Instação?")
+    _allss_nome = fields.Char("Nome")
     # _allss_wage = fields.Monetary("Salário") 
     # _allss_health_insurance = fields.Boolean("Convênio Médico")
     # _allss_date_exped_cnh = fields.Date("Data de Expedição")
