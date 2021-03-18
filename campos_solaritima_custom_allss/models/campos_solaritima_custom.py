16ca11802c78d208213665425734b372d19a283c
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2021-03-18 10:57:18 -0300

    Testes

--- campos_solaritima_custom_allss/models/campos_solaritima_custom.py
+++ campos_solaritima_custom_allss/models/campos_solaritima_custom.py
@@ -7,9 +7,17 @@ class ResPartnerCustom(models.Model):
     _inherit = "res.partner"
 
     currency_id = fields.Many2one("res.currency", related="company_id.currency_id")
-    _allss_concessionaria = fields.Char("Concessionaria")
+    # GRUPO: CLIENTE
+    _allss_fornecedor = fields.Char("Fornecedor")
     _allss_titular_inst = fields.Char("Copiar Cliente para Titular da Instação?")
     _allss_nome = fields.Char("Nome")
+    _allss_cpf = fields.Char("CPF do Cliente")
+    # GRUPO: DADOS DA INSTALAÇÃO
+    _allss_concessionaria = fields.Char("Concessionaria")
+
+
+
+    
     # _allss_wage = fields.Monetary("Salário") 
     # _allss_health_insurance = fields.Boolean("Convênio Médico")
     # _allss_date_exped_cnh = fields.Date("Data de Expedição")
