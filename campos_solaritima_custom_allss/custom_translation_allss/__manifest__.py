560b71afe56151718e43f982eaeb678f60ff131a
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2021-04-13 17:05:23 -0300

    Teste Módulo de Tradução

---
+++ campos_solaritima_custom_allss/custom_translation_allss/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Translation ALLSS',
+    'version': '14.0.0.0.0',
+    'licence': 'AGPL-3',
+    'category': '',
+    'author': 'Renata (ALLSS Soluções em Sistemas)',
+    'summary': '',
+    'description':'Translation',
+    'depends': [
+        '',
+    ],
+    'data': [
+        #views
+    ],
+    'demo': [
+    ],
+    'css': [],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
