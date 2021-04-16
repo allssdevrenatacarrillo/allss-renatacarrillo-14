99fc8e1ba42fae94f4d90ffbc794ce660f432812
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2021-04-16 11:38:43 -0300

    ATUALIZAÇÃO res.partner 02

--- dev_allss_partner/models/campos_allss_custom.py
+++ dev_allss_partner/models/campos_allss_custom.py
@@ -7,14 +7,13 @@ class ResPartnerCustom(models.Model):
     _inherit = "res.partner"
 
     currency_id = fields.Many2one("res.currency", related="company_id.currency_id")
-    # GRUPO: CLIENTE
     _allss_cnpj = fields.Char("CNPJ")
     _allss_insc_est = fields.Char("Inscrição Estadual")
     _allss_cnae = fields.Char("CNAE")
-    # _allss_contrib = fields.Selection("Contribuinte" selection_add=[
-    #     ('sim', 'Sim'),
-    #     ('não', 'Não'),
-    # ])
+    _allss_contrib = fields.Selection("Contribuinte" selection_add=[
+        ('sim', 'Sim'),
+        ('não', 'Não'),
+    ])
     _allss_bairro = fields.Char("Bairro")
     _allss_cod_mun_ibge = fields.Char("Cód. Município do IBGE")
     _allss_coord = fields.Many2many('res.partner', "Coordenador")
