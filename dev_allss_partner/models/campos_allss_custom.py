abed295ca5ba5ba2fd91f466d6c2f1dd81ef16d4
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2021-04-16 12:37:07 -0300

    ATUALIZAÇÃO res.partner 11

--- dev_allss_partner/models/campos_allss_custom.py
+++ dev_allss_partner/models/campos_allss_custom.py
@@ -13,7 +13,7 @@ class ResPartnerCustom(models.Model):
     _allss_contrib = fields.Boolean("Contribuinte")
     _allss_bairro = fields.Char("Bairro")
     _allss_cod_mun_ibge = fields.Char("Cód. Município do IBGE")
-    _allss_coord = fields.Many2many('res.partner', "Coordenador")
+    _allss_coord = fields.Many2many("Coordenador")
 
 
     
