b8aa61b572ad86450c3512eff7caa92b958b4123
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2021-04-16 10:43:12 -0300

    campos allss

---
+++ dev_allss_partner/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Campos Adicionais Solaritima',
+    'version': '14.0.1.0.0',
+    'licence': 'AGPL-3',
+    'category': 'Partner',
+    'author': 'Renata Carrillo (ALLSS Soluções em Sistemas)',
+    'summary': 'Res Partner',
+    'description':'Campos Adicionais ALLSS',
+    'depends': [
+        'contacts',
+    ],
+    'data': [
+        #views
+        'views/campos_allss_custom.xml',
+    ],
+    'demo': [
+    ],
+    'css': [],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
