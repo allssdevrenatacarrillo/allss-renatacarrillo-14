0bdb8c57a0d383850b1c6302aee3df77678d0fba
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2021-04-22 15:52:24 -0300

    Testando Módulo de Tradução

---
+++ translation_allss-11.0/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# © 2016 Helton Penariol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Translation',
+    'description': 'This module is used for a translation of the modules',
+    'category': 'Translation',
+    'license': 'AGPL-3',
+    'author': 'Helton Penariol',
+    'website': '',
+    'version': '11.0.1.0.0',
+    'depends': ['base'],
+    'installable': True,
+}
