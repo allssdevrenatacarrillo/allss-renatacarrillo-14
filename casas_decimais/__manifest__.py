093c746d9faa14dbf23602f4564245c07f1281c7
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2021-04-28 17:29:45 -0300

    Módulo Casas Decimais - Imposto

--- casas_decimais/__manifest__.py
+++ casas_decimais/__manifest__.py
@@ -10,12 +10,7 @@
     'depends': [
         'account_accountant',
     ],
-    'data': [
-        #views
-        # 'views/teste_casa_decimal.xml',
-        # 'views/account.tax.form.xml',
-        # 'views/account.view_tax_form.xml',
-    ],
+    'data': [],
     'demo': [
     ],
     'css': [],
