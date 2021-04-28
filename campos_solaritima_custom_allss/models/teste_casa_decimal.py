72976f78666f0ca5ac5070f6aaee910b143deb93
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2021-04-28 16:10:16 -0300

    teste casa decimal - solaritima

---
+++ campos_solaritima_custom_allss/models/teste_casa_decimal.py
@@ -0,0 +1,10 @@
+from odoo import fields, models, api, _
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class ResPartnerCustom(models.Model):
+    _inherit = "account.tax"
+
+    currency_id = fields.Many2one("res.currency", related="company_id.currency_id")
+    _decimal_teste = fields.Float('Montante TESTE', default=1, store=True, digits=(12,11))
