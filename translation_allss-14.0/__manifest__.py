7743dedec027a109f7f898a7a32b04695872c74c
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2021-05-05 12:20:20 -0300

    atualização módulo de tradução

---
+++ translation_allss-14.0/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Translation Custom ALLSS',
+    'version': '14.0.0.0.0',
+    'licence': 'AGPL-3',
+    'category': 'Translate',
+    'author': 'Renata (ALLSS Soluções em Sistemas)',
+    'summary': '',
+    'description':'Translation',
+    'depends': [
+        'base',
+        'planning'
+    ],
+    'data': [
+        #views
+    ],
+    'demo': [
+    ],
+    'css': [],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
