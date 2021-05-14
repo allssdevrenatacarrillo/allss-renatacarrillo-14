d6d4551efee77d8aa6bff7fcafbf3d5a9b4bd8fc
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2021-05-14 12:05:47 -0300

    teste módulo vendor 01

--- custom_allss_vendor/models/vendor_custom.py
+++
@@ -1,9 +0,0 @@
-# from odoo import fields, models, api, _
-# import logging
-
-# _logger = logging.getLogger(__name__)
-
-# class ResPartnerCustom(models.Model):
-#     _inherit = "res.users"
-
-#     _allss_teste_vendor = fields.Char("Vendedor")
