bbc430c43dc89251cba753976037e1f32ae8faca
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2021-05-18 16:34:08 -0300

    teste 18/05 08

---
+++ custom_project_vendor/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Campo Vendedor na Tarefa do Projeto',
+    'version': '12.0.1.0.0',
+    'licence': 'AGPL-3',
+    'category': 'Partner',
+    'author': 'Renata Carrillo (ALLSS Soluções em Sistemas)',
+    'summary': 'Project',
+    'description':'Campo Vendedor na Tarefa do Projeto',
+    'depends': [
+        'contacts',
+        'project',
+        'base',
+    ],
+    'data': [
+        #views
+        'views/campos_custom_project_vendor.xml',
+    ],
+    'demo': [
+    ],
+    'css': [],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
