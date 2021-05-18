ad2c3a344c56721ab2556638fc7bbbce5997f70b
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2021-05-18 13:10:08 -0300

    teste 18/05 03

---
+++ custom_allss_vendor_project/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Vendedor em Tarefas do Projeto',
+    'version': '12.0.1.0.0',
+    'licence': 'AGPL-3',
+    'category': 'Partner',
+    'author': 'Renata Carrillo (ALLSS Soluções em Sistemas)',
+    # 'summary': 'Res Partner',
+    'description':'Campos Adicionais ALLSS',
+    'depends': [
+        'contacts',
+        'project',
+        'sale',
+    ],
+    'data': [
+        #views
+        # 'views/custom_allss_vendor.xml',
+    ],
+    'demo': [
+    ],
+    'css': [],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
