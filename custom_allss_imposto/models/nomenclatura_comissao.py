177dfb6e3a9b984496acddf36a0377ee69ffeade
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2021-05-20 09:21:33 -0300

    módulo que altera nomemclatura imposto para comissão 02

--- custom_allss_imposto/models/nomenclatura_comissao.py
+++ custom_allss_imposto/models/nomenclatura_comissao.py
@@ -6,4 +6,4 @@ _logger = logging.getLogger(__name__)
 class ResPartnerCustom(models.Model):
     _inherit = "sale.order"
 
-    amount_tax = fields.Float("Comissão")
+    amount_tax = fields.Float('Comissão', default=1, store=True)
