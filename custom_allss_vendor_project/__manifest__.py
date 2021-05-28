7aa1fe873dd802161964aecb850b6f72815069cd
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2021-05-28 13:07:54 -0300

    módulo vendedor no projeto

---
+++ custom_allss_vendor_project/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Vendedor no Projeto',
+    'version': '14.0.1.0.0',
+    'licence': 'AGPL-3',
+    'category': '',
+    'author': 'Renata Carrillo (ALLSS Soluções em Sistemas)',
+    'summary': '',
+    'description':'Vendedor referenciado no módulo de Projeto',
+    'depends': [
+        'account_accountant',
+        'sale_management',
+        'account',
+        'project',
+    ],
+    'data': [],
+    'demo': [],
+    'css': [],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+}
