08bb234656df7c3cfcc8961ef0156a7f1d7d55b8
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2021-05-28 14:02:43 -0300

    módulo vendedor no projeto 18

--- custom_allss_vendor_project/models/vendor_project.py
+++ custom_allss_vendor_project/models/vendor_project.py
@@ -6,4 +6,4 @@ _logger = logging.getLogger(__name__)
 class ResPartnerCustom(models.Model):
     _inherit = "sale.order"
 
-    user_id = fields.Many2one('res.users', string='Salesperson', index=True, track_visibility='onchange', track_sequence=2, default=lambda self: self.env.user)
+    _allss_user_id = fields.Many2one('res.users', string='Salesperson', index=True, track_visibility='onchange', track_sequence=2, default=lambda self: self.env.user)
