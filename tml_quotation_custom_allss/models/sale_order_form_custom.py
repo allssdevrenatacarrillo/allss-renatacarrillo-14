ffe20e3d78583e767c58601a0f7dae9b7be831ba
allssdevcaiqueanastacio <caique.anastacio@allss.com.br>
2021-05-28 11:15:07 -0300

    [FIX] multiplicador por unidade

--- tml_quotation_custom_allss/models/sale_order_form_custom.py
+++ tml_quotation_custom_allss/models/sale_order_form_custom.py
@@ -23,8 +23,7 @@ class SaleOrder(models.Model):
             for acres in line.acres_ids:
                 acres_line += line.price_unit / 100 * acres.amount * line.product_uom_qty
             for comiss in line.comiss_ids:
-                comiss_line += line.price_unit / 100 * comiss.amount
-                comiss_line * line.product_uom_qty
+                comiss_line += line.price_unit / 100 * comiss.amount * line.product_uom_qty
         self.update({
             'acres_total': acres_line,
             'comiss_total': comiss_line,
