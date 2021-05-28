bbd8ac29e937bd3b1e4b9e17ec4e85517a72b088
allssdevrenatacarrillo <renata.carrillo@allss.com.br>
2021-05-28 13:27:24 -0300

    módulo vendedor no projeto 04

--- custom_allss_vendor_project/models/vendor_project.py
+++ custom_allss_vendor_project/models/vendor_project.py
@@ -6,4 +6,4 @@ _logger = logging.getLogger(__name__)
 class ResPartnerCustom(models.Model):
     _inherit = "res.users"
 
-    _allss_user_id = fields.Many2one("Vendedor")
+    _allss_user_id = fields.Char("Vendedor")
